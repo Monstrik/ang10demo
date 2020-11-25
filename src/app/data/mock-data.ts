97b73c7d56f59fbc4c33bf91da61287d16dd9132
Alexandr <IamWebDev@gmail.com>
2020-11-25 18:40:20 -0500

    add Post Interface and MockData

---
+++ src/app/data/mock-data.ts
@@ -0,0 +1,16 @@
+import {Post} from './Post';
+
+export const POSTS: Post[] = [
+  {
+    id: '1',
+    title: 'This is an item',
+    text: 'This is a description of the item, it might describe a bug/task/comment, it can also display <a href=”​www.google.com”​ >Links</a>',
+    tags: ['bug', 'issue', 'etc'],
+  },
+  {
+    id: '2',
+    title: 'This is an item 2',
+    text: 'This is a description of the item 2, it might describe a bug/task/comment, it can also display <a href=”​www.google.com”​ >Links</a>',
+    tags: ['bug', 'issue', 'etc'],
+  },
+];
