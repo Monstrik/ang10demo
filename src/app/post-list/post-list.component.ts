010c2f720281893e484df914f357c78e57f16703
Alexandr <IamWebDev@gmail.com>
2020-11-25 18:45:14 -0500

    add PostList Component

---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {POSTS} from '../data/mock-data';
+import {Post} from '../data/Post';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss']
+})
+export class PostListComponent implements OnInit {
+
+  constructor() { }
+
+  posts = POSTS;
+  selectedPost: Post;
+
+  ngOnInit(): void {
+  }
+
+
+  onSelect(post: Post): void {
+    this.selectedPost = post;
+    console.log('this.selectedPost=', this.selectedPost);
+  }
+}
